bb915dd2c2281a426078dbd2d7a3a6ab7d9ac19b
Concision <contact@concision.me>
2020-10-23 19:13:11 -0700

    feat: resolved eslint warnings/errors

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import AcornStage3Plugin from "acorn-stage3";
 import {SnowpackConfig, SnowpackPlugin, SnowpackPluginFactory} from "snowpack";
 import {InputOptions, Plugin as RollupPlugin} from "rollup";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface PluginOptions {
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default <SnowpackPluginFactory<PluginOptions>>((snowpackConfig: SnowpackConfig, pluginOptions?: PluginOptions): SnowpackPlugin => {
     return {
         name: "snowpack-acorn-stage3-plugin",
         config(snowpackConfig: SnowpackConfig): void {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             (snowpackConfig.installOptions.rollup!.plugins ??= []).push(<RollupPlugin>{
                 name: "rollup-acorn-stage3-plugin",
                 options: (options: InputOptions): InputOptions => {
